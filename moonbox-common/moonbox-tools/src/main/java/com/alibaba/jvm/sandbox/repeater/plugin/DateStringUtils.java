146e63c64d6a873e2fdae8a9b5fea101314a9754
yananliuyanjiang <2489346416@qq.com>
2023-01-06 09:45:49 +0800

    Update DateStringUtils.java

--- moonbox-common/moonbox-tools/src/main/java/com/alibaba/jvm/sandbox/repeater/plugin/DateStringUtils.java
+++ moonbox-common/moonbox-tools/src/main/java/com/alibaba/jvm/sandbox/repeater/plugin/DateStringUtils.java
@@ -52,10 +52,6 @@ public class DateStringUtils {
      */
     private static String DATETIME_REGEX2 ="202[2-9][0-1]{1}[0-9]{1}[0-3]{1}[0-9]{1}[0-2]{1}[0-9]{1}[0-5]{1}[0-9]{1}[0-5]{1}[0-9]{1}";
 
-    /**
-     * 解析来自requestHeaders的date
-     * [Accept: application/json, User-Agent: volc-sdk-java/v1.0.10, Content-Type: application/json, Host: open.volcengineapi.com, X-Date: 20220420T031448Z, X-Content-Sha256: 9c53a29b036fd2c20af26f63f0f01a3b3ba4fa9e68570d2fd915befa8e52ea74, Authorization: HMAC-SHA256 Credential=AKLTZDhmOGVmMzA5N2M3NDU1M2JmMjVhNzZiNWFmODQ4NTU/20220420/cn-north-1/CreativeManagementPlatform/request, SignedHeaders=content-type;host;x-content-sha256;x-date, Signature=31b01e5964485672b86ee0b80b4b64c7ce4d51a9e3314e48bcf97df7f7442ec7]
-     */
     private static String DATETIME_REGEX3 ="202[2-9][0-1]{1}[0-9]{1}[0-3]{1}[0-9]{1}T[0-2]{1}[0-9]{1}[0-5]{1}[0-9]{1}[0-5]{1}[0-9]{1}Z";
 
     /**
