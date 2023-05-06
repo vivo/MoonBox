0cdf8b21c52ac356e5962c643f34671cac8efc71
liuzhe <liuzhe4@sfmail.sf-express.com>
2023-05-06 15:50:43 +0800

    修复如果子调用如果忽略多个字段时，只有一个子调用生效的问题。

--- moonbox-agent/moonbox-java-agent/moonbox-core/src/main/java/com/alibaba/jvm/sandbox/repeater/plugin/core/impl/spi/AbstractReflectCompareStrategy.java
+++ moonbox-agent/moonbox-java-agent/moonbox-core/src/main/java/com/alibaba/jvm/sandbox/repeater/plugin/core/impl/spi/AbstractReflectCompareStrategy.java
@@ -2,14 +2,12 @@ package com.alibaba.jvm.sandbox.repeater.plugin.core.impl.spi;
 
 import java.util.ArrayList;
 import java.util.Comparator;
-import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
-import java.util.stream.Collectors;
 
 import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.lang3.StringUtils;
